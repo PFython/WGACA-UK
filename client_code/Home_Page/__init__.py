afc3cae781af686c238d4671212f0a5d6d94cf00
WGACA@southwestlondon.tv <WGACA@southwestlondon.tv>
2020-03-30 10:16:33 +0000

    Saved automatically

--- client_code/Home_Page/__init__.py
+++ client_code/Home_Page/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import Home_PageTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 ITEM_HEIRARCHY = ('Food | Vegetables | Tomatoes',
               'Food | Vegetables | Potatoes',
