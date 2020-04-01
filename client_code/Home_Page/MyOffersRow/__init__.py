3742221a4abb6357d9f38af16e4df058530afc34
WGACA@southwestlondon.tv <WGACA@southwestlondon.tv>
2020-04-01 09:00:39 +0000

    Saved automatically

--- client_code/Home_Page/MyOffersRow/__init__.py
+++ client_code/Home_Page/MyOffersRow/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import ItemTemplate1Template
+from ._anvil_designer import MyOffersRowTemplate
 from anvil import *
 import anvil.microsoft.auth
 import anvil.google.auth, anvil.google.drive
@@ -10,10 +10,21 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class ItemTemplate1(ItemTemplate1Template):
+class MyOffersRow(MyOffersRowTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-
         # Any code you write here will run when the form opens.
-    
+
+    def delete_row_click(self, **event_args):
+        """This method is called when the link is clicked"""
+        self.item.delete()
+        self.remove_from_parent()
+
+    def show_notes_click(self, **event_args):
+        """This method is called when the link is clicked"""
+        text = "\nNOTES:\n\n" + self.item['notes']
+        alert(text)
+
+
+
--- client_code/Home_Page/UserProfile/__init__.py
+++ client_code/Home_Page/UserProfile/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import User_ProfileTemplate
+from ._anvil_designer import UserProfileTemplate
 from anvil import *
 import anvil.server
 import anvil.microsoft.auth
@@ -10,7 +10,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class User_Profile(User_ProfileTemplate):
+class UserProfile(UserProfileTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
