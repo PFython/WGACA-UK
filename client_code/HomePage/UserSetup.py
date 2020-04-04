1d2746946380625772ab066187eaf613a9e97d0a
WGACA@southwestlondon.tv <WGACA@southwestlondon.tv>
2020-04-04 19:32:34 +0000

    Saved automatically

--- client_code/HomePage/UserSetup.py
+++ client_code/HomePage/UserSetup.py
@@ -19,28 +19,38 @@ class UserSetup(UserSetupTemplate):
         self.display_name.text = user['display_name']
         self.email.text = user['email']
         self.house_number.text = user['house_number']
-        if user['street']:
-            self.street.selected_value = user['street']
-        if user['town']:
-           self.town.selected_value = user['town']
+        # Create list of valid counties
         self.county.items = sorted(self.addresses.keys())
         if user['county']:
             self.county.selected_value = user['county']
         else:
             self.county.selected_value = sorted(list(self.addresses.keys()))[0]
             self.county_change()
+        # Create list of valid streets
+        self.street.items = self.get_streets_from_county()
+        if user['street']:           
+            self.street.selected_value = user['street']
+        else:
+            self.street_change()
+        if user['town']:
+           self.town.selected_value = user['town']
         if not user['country']:
             anvil.server.call("save_user_setup", 'country', LOCALE)
         self.country.text = user['country']
-        self.telephone.text = user['telephone'] 
-  
-    def county_change(self, **event_args):
-        """This method is called when an item is selected"""
+        self.telephone.text = user['telephone']
+        
+    def get_streets_from_county(self):
         towns = UserSetup.addresses[self.county.selected_value]
         streets = []
         for town in towns:
             streets.extend(UserSetup.addresses[self.county.selected_value][town])
         streets.sort()
+        print(streets)
+        return streets
+  
+    def county_change(self, **event_args):
+        """This method is called when an item is selected"""
+        streets = self.get_streets_from_county()
         self.street.items = streets
         self.street.selected_value = streets[0]
         self.street_change()
@@ -51,6 +61,7 @@ class UserSetup(UserSetupTemplate):
         for town, street_list in towns.items():
             if self.street.selected_value in street_list:
                 break
+        # Create single item list of valid towns        
         self.town.items = [town]
         self.town.selected_value = town
         
