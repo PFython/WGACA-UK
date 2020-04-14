54d5188bd9224d7290b7ba161fcee9318b02db04
WGACA@southwestlondon.tv <WGACA@southwestlondon.tv>
2020-04-14 23:15:35 +0000

    Saved version

--- client_code/HomePage/MyOffers/MyOffersRow/__init__.py
+++ client_code/HomePage/MyOffers/MyOffersRow/__init__.py
@@ -20,17 +20,20 @@ class MyOffersRow(MyOffersRowTemplate):
         self.check_offer_status()     
 
     def check_offer_status(self, **properties):
-        matches = anvil.server.call('get_my_matches')
-        match_count = 0
-        for match in matches:
-            if match['request'] == self.item:
-                match_count += 1
+        match_count = len(self.item['matches'])
+        
+#         matches = anvil.server.call('get_my_matches')
+#         match_count = 0
+#         for match in matches:
+#             if match['request'] == self.item:
+#                 match_count += 1
+                
         print ("Matches:",match_count)
-        if match_count > 0 and match['status_code'] in ['1','2']:                
+        if match_count > 0 and self.item['status_code'] in ['1','2']:                
             self.status.text = f"Matched with {match_count} requests.  Please check My Matches."
 #             old_colour = self.parent.parent.parent.parent.border
 #             print(old_colour)
-            self.parent.background = red
+            self.parent.parent.background = red
 #             menu_my_matches.border = red
         self.refresh_data_bindings()
         
@@ -50,6 +53,14 @@ class MyOffersRow(MyOffersRowTemplate):
       self.status.text = STATUSES[self.item['status_code']]
       self.status.foreground = '#0080c0' if self.status.text.startswith("New") else '#5eb348'
       self.check_offer_status()
+
+    def click_status(self, **event_args):
+      """This method is called when the link is clicked"""
+        self.parent.clear()
+        # Add Page1 to the content panel
+        self.column_panel_1.add_component(Matches())
+        self.highlight_selected_menu(self.menu_my_matches)
+
 
 
 
