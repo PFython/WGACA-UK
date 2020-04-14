a6d2cd0e15adad5b2de1af229caea851655feb27
WGACA@southwestlondon.tv <WGACA@southwestlondon.tv>
2020-04-14 16:54:09 +0000

    Saved automatically

--- client_code/KarmaForm.py
+++ client_code/KarmaForm.py
@@ -12,8 +12,9 @@ class KarmaForm(KarmaFormTemplate):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
         # Any code you write here will run when the form opens.
+        self.date.pick_time = True
         self.date.date = datetime.datetime.today()
-        self.date.format = "%D %b %Y"
+        self.date.format = "D %b %Y"
 
 
     def send_message(self, **event_args):
@@ -37,6 +38,19 @@ class KarmaForm(KarmaFormTemplate):
     def description_change(self, **event_args):
       """This method is called when the text in this text area is edited"""
       pass
+
+    def select_star(self, **event_args):
+      """This method is called when a Star link is clicked"""
+      stars = {self.star_5: self.label_5,
+              self.star_4: self.label_4,
+              self.star_3: self.label_3,
+              self.star_2: self.label_2,
+              self.star_1: self.label_1}
+      for star in stars:
+          star.icon = 'fa:star-o'
+      event_args['sender'].icon = 'fa:star'
+      rating = stars[event_args['sender']].text
+      print(rating)
 
 
 
