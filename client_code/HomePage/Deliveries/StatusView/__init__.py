7c772791b9296600ada8729addc9f91b1d31a6cb
WGACA@southwestlondon.tv <WGACA@southwestlondon.tv>
2020-04-16 10:45:37 +0000

    Saved version

--- client_code/HomePage/Deliveries/StatusView/__init__.py
+++ client_code/HomePage/Deliveries/StatusView/__init__.py
@@ -17,6 +17,7 @@ class StatusView(StatusViewTemplate):
 #         self.is_runner.checked = self.user == self.match['approved_runner']
 #         self.is_requester.checked = self.user == self.match['request']['user']
         self.all_checkboxes = [x for x in self.card_1.get_components() if type(x) == CheckBox]
+        self.all_arrows = [x for x in [x for x in self.card_1.get_components() if type(x) == Label] if x.icon == 'fa:arrow-down']
         self.test_setup()
         self.initial_canvas()
         self.initial_options_by_role()
@@ -26,8 +27,7 @@ class StatusView(StatusViewTemplate):
         self.match = anvil.server.call('_get_all_matches')[0] # Test data
         for x in (self.is_offerer, self.is_requester, self.is_runner):
             x.enabled = True
-            x.set_event_handler('change', self.initial_options_by_role)
-            
+            x.set_event_handler('change', self.initial_options_by_role)     
         
         
     def initial_canvas(self):
@@ -54,26 +54,30 @@ class StatusView(StatusViewTemplate):
         
     def initial_options_by_role(self, **event_args):
         components = set(self.card_1.get_components())
-        if self.is_runner.checked:
+        # Single Roles
+        if self.is_runner.checked and not self.is_offerer.checked and not self.is_requester.checked:
             components.update({self.runner_confirms_pickup, self.runner_confirms_dropoff, self.dropoff_agreed, self.pickup_agreed})
-        if self.is_offerer.checked:
+        if self.is_offerer.checked and not self.is_runner.checked:
             components.update({self.offerer_confirms_pickup, self.pickup_agreed})
-        if self.is_requester.checked:
+        if self.is_requester.checked and not self.is_runner.checked:
             components.update({self.requester_confirms_dropoff, self.dropoff_agreed})
         for component in components:
             component.enabled = True
         for checkbox in self.all_checkboxes:
             checkbox.set_event_handler("change", self.refresh_canvas)
             checkbox.sticky = True
+        # Offerer=Runner       
         visible = False if self.is_offerer.checked and self.is_runner.checked else True
         for checkbox in self.all_checkboxes[:7]:
             checkbox.visible = visible
-            self.feedback_on_runner_by_offerer.visible = False
-        else:
-          
-        if self.is_runner.checked and self.is_requester.checked:
-            self.feedback_on_requester_by_runner.visible= False
-            self.feedback_on_runner_by_requester.visible = False
+        for arrow in self.all_arrows[:4]:
+            arrow.visible = visible
+        # Requester=Runner    
+        visible = False if self.is_runner.checked and self.is_requester.checked else True
+        for checkbox in self.all_checkboxes[7:12]:
+            checkbox.visible = visible
+        for arrow in self.all_arrows[-3:]:
+            arrow.visible = visible
         
     def refresh_canvas(self, **event_args):
         self.sender = event_args.get('sender')
