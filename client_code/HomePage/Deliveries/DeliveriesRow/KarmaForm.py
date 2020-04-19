f5e3264dc6cc645a8ea70dfb23195b686f44961b
WGACA@southwestlondon.tv <WGACA@southwestlondon.tv>
2020-04-19 17:15:13 +0000

    Ditto

--- client_code/HomePage/Deliveries/DeliveriesRow/KarmaForm.py
+++ client_code/HomePage/Deliveries/DeliveriesRow/KarmaForm.py
@@ -24,7 +24,7 @@ class KarmaForm(KarmaFormTemplate):
     def add_footer(self):
         """Adds details of the person giving feedback and the person who it's about"""
         footer = f"\n[{self.regarding.text} was the {self.regarding_role.text}\n"
-        footer += f"{self.user} was the {self.user_role.text}]"
+        footer += f"{self.user['display_name']} was the {self.user_role.text}]"
         print(footer)
         return footer
     
