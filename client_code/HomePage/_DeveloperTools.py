f73467728dc896f488239d059ee94a5b5cbac2ed
WGACA@southwestlondon.tv <WGACA@southwestlondon.tv>
2020-04-19 08:26:58 +0000

    Saved automatically

--- client_code/HomePage/_DeveloperTools.py
+++ client_code/HomePage/_DeveloperTools.py
@@ -18,14 +18,3 @@ class _DeveloperTools(_DeveloperToolsTemplate):
         """This method is called when the button is clicked"""
         anvil.server.call('_backfill_approx_lon_lat')
 
-    def click_limit_status_codes(self, **event_args):
-        """This method is called when the button is clicked"""
-        limit = self.limit_amount.text
-        count = 0
-        matches = anvil.server.call('_get_all_matches')
-        for match in anvil.server.call('_get_all_matches'):
-            match_value = match['status_code'] or limit
-            if int(match_value) >= int(limit):
-                anvil.server.call("update_status_codes", match, limit)
-                count += 1
-        print(f"{count} matches out of {len(matches)} limited to status code [{limit}]")
