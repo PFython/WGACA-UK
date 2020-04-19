3b54a3fc28e1e5dcc5d0281ee42d646b6c9da257
WGACA@southwestlondon.tv <WGACA@southwestlondon.tv>
2020-04-19 11:51:45 +0000

    Ditto

--- client_code/HomePage/Matches/MatchesRow/ConfirmMatch.py
+++ client_code/HomePage/Matches/MatchesRow/ConfirmMatch.py
@@ -17,6 +17,7 @@ class ConfirmMatch(ConfirmMatchTemplate):
         self.init_components(**properties)
         self.requester = requester
         self.runner_dropdown.items = runners
+        self.user = anvil.users.get_user()
         # Any code you write here will run when the form opens.
 
     def dropdown_change(self, **event_args):
@@ -26,11 +27,6 @@ class ConfirmMatch(ConfirmMatchTemplate):
             event_args['sender'].background = pink
         else:
             event_args['sender'].background = white
-            user = anvil.users.get_user()
-            if runner.replace(" (myself)","") != user['display_name']:
-                self.telephone.checked = runner in [x['display_name'] for x in (user['telephone_shared_with'] or [])]
-                self.email.checked = runner in [x['display_name'] for x in (user['email_shared_with'] or [])]
-                self.postcode.checked = runner in [x['display_name'] for x in (user['postcode_shared_with'] or [])]
         self.confirm_match_button.enabled = self.runner_dropdown.selected_value in self.runner_dropdown.items
         self.refresh_data_bindings()
 
@@ -42,10 +38,9 @@ class ConfirmMatch(ConfirmMatchTemplate):
         self.visible = False
 
     def confirm_match_button_click(self, **event_args):
-        """This method is called when the Confirm Match button is clicked"""
-        user = anvil.users.get_user()
+        """This method is called when the Confirm Match button is clicked"""      
         runner = anvil.server.call("get_user_from_display_name", self.runner_dropdown.selected_value.replace(" (myself)",""))
-        self.update_shared_with_fields(user, runner)
+        self.update_shared_with_fields(self.user, runner)
         self.update_databases(runner)
         self.parent.parent.parent.show_myself()
         self.parent.parent.visible = False
@@ -55,7 +50,7 @@ class ConfirmMatch(ConfirmMatchTemplate):
     def add_remove_sharing(self, user, dictionary, recipient):
         """ Adds or Removes a user in the shared_with column """
         for field, checked in dictionary.items():
-            users = list(set((user[field] or []) + [recipient]))
+            users = list(set((self.user[field] or []) + [recipient]))
             if not checked:
                 users = users.remove(recipient)
             anvil.server.call('save_user_setup', field, users)
@@ -66,8 +61,8 @@ class ConfirmMatch(ConfirmMatchTemplate):
         sharing_dict = {'telephone_shared_with': self.telephone.checked,
                       'email_shared_with': self.email.checked,
                       'postcode_shared_with': self.postcode.checked,}
-        self.add_remove_sharing(user, sharing_dict, runner)
-        self.add_remove_sharing(user, sharing_dict, requester)
+        self.add_remove_sharing(self.user, sharing_dict, runner)
+        self.add_remove_sharing(self.user, sharing_dict, requester)
              
     def update_databases(self, runner):
         """ Sets Approved Runner, updates Matches/Offers/Requests, and refreshes the view """
