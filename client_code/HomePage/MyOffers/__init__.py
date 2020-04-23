f39235c681ebb7b07322a36da5d8d23913d805b7
WGACA@southwestlondon.tv <WGACA@southwestlondon.tv>
2020-04-23 09:27:24 +0000

    +Filters by area for Offers

--- client_code/HomePage/MyOffers/__init__.py
+++ client_code/HomePage/MyOffers/__init__.py
@@ -24,9 +24,9 @@ class MyOffers(MyOffersTemplate):
                               self.radio_button_2: self.user['street'],
                               self.radio_button_3: self.user['town'],
                               self.radio_button_4: self.user['county'],}
-        self.radio_button_2.value = f"Items in {self.user['street'] or 'my Street'}"
-        self.radio_button_3.value = f"Items in {self.user['town'] or 'my Town'}"
-        self.radio_button_4.value = f"Items in {self.user['county'] or 'my County'}"
+        self.radio_button_2.text = f"Items in {self.user['street'] or 'my Street'}"
+        self.radio_button_3.text = f"Items in {self.user['town'] or 'my Town'}"
+        self.radio_button_4.text = f"Items in {self.user['county'] or 'my County'}"
         if self.user['county_view']:
             self.radio_button_4.visible = True
         anvil.server.call('generate_matches')
@@ -62,7 +62,6 @@ class MyOffers(MyOffersTemplate):
     def radio_button_clicked(self, **event_args):
       """This method is called when this radio button is selected"""
       sender = event_args['sender']
-      self.refresh_data_bindings()
       for button in self.radio_buttons:
           button.bold = True if button.selected else False
 
