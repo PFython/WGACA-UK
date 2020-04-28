a0563f8db983a85507a4fe820382f85506a27ba4
WGACA@southwestlondon.tv <WGACA@southwestlondon.tv>
2020-04-28 13:26:41 +0000

    Saved automatically

--- client_code/AutoCompOption/__init__.py
+++ client_code/AutoCompOption/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import ItemTemplate1Template
+from ._anvil_designer import AutoCompOptionTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -6,7 +6,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class ItemTemplate1(ItemTemplate1Template):
+class AutoCompOption(AutoCompOptionTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
